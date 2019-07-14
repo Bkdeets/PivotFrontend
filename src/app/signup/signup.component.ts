926a537179f07741fe6c71136784105cea10d791
Britton <brittonkdeets@gmail.com>
2019-07-14 10:18:26 -0400

    signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+    userSignup: FormGroup;
+    loading: Boolean;
+    // UserService = new UserService();
+
+  constructor(
+      private router: Router,
+      private fb: FormBuilder) {
+
+  }
 
   ngOnInit() {
+      this.userSignup = this.fb.group({
+          username: ['', Validators.required],
+          password: ['', Validators.required]
+      });
   }
+
+  goHome() {
+      this.router.navigate(['/home']);
+  }
+
+  onLogin() {
+      return
+  }
+
+  // onLogin() {
+  //   let token = this.UserService.getUserToken(this.userLogin.value.username, this.userLogin.value.password);
+  //   if (token) {
+  //       // redirect to profile page of given user token
+  //   } else {
+  //       this.error = 'invalid creds bro';
+  //   }
+  // }
 
 }
