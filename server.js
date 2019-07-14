2600e0af8df8eda14738f20aa88e01c0737f1130
Britton <brittonkdeets@gmail.com>
2019-07-14 09:26:18 -0400

    renaming app

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/TradingAppFrontend'));
+app.use(express.static(__dirname + '/dist/Pivot'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/TradingAppFrontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Pivot/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
