49d22b0d617af0ec476a35905f00dd835d73051e
Britton <brittonkdeets@gmail.com>
2019-08-09 22:44:55 -0400

    ...

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UserService } from '../services/user.service';
-import { Wrapper } from './tideline-backend-api-wrapper/wrapper';
+import { Wrapper } from 'src/app/tideline-backend-api-wrapper/wrapper';
 
 
 @Component({
